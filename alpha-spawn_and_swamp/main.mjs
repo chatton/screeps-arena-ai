33f8a0b806b0f18cd3512f7527d2c08f35c1da3a
chatton <github.qpeyb@simplelogin.fr>
2024-12-07 22:38:15 +0000

    chore: updated to state machine design

--- alpha-spawn_and_swamp/main.mjs
+++ alpha-spawn_and_swamp/main.mjs
@@ -3,122 +3,136 @@ import {StructureSpawn, Creep, StructureContainer} from 'game/prototypes';
 import {MOVE, WORK, CARRY, ATTACK, RESOURCE_ENERGY, ERR_NOT_IN_RANGE, RANGED_ATTACK, HEAL, TOUGH} from 'game/constants';
 import {} from 'arena';
 
+const ROLE_HARVESTER = "harvester";
+const ROLE_RANGED_ATTACKER = "ranged_attacker";
 
-const squadNum = 4;
-const squads = [];
+// spawnStateMachine is the state machine which controls spawning of units, it must be initialized in the first tick.
+let spawnStateMachine;
+// creepStateMachine is the state machine which controls the movements and actions of creeps, it must be initialized in the first tick.
+let creepStateMachine;
+
+function initSpawnStateMachine() {
+    const spawn = getObjectsByPrototype(StructureSpawn).find(s => s.my);
+
+    const sm = new StateMachine();
+    sm.setStateTransition("harvester state", () => getObjectsByPrototype(Creep).filter(c => c.my && c.job === ROLE_HARVESTER).length < 3, new SpawnStateHarvester(spawn))
+    sm.setStateTransition("ranged attacker state", () => true, new SpawnStateRangedAttacker(spawn))
+    return sm;
+}
+
+function initCreepStateMachine() {
+    const sm = new StateMachine();
+    sm.setStateTransition("gather initial resources", () => true, new CreepStateGatherInitialResources());
+    return sm;
+}
 
 export function loop() {
-    const harvesters = getObjectsByPrototype(Creep).filter(c => c.my && c.job == "harvester");
-    // const attackers = getObjectsByPrototype(Creep).filter(c => c.my && c.job == "attacker");
-    const enemyCreeps = getObjectsByPrototype(Creep).filter(c => !c.my);
-    const spawn = getObjectsByPrototype(StructureSpawn).find(s => s.my);
-    const enemySpawn = getObjectsByPrototype(StructureSpawn).find(s => !s.my);
-    const containers = getObjectsByPrototype(StructureContainer).filter(c => c.store[RESOURCE_ENERGY] <= c.store.getCapacity() && c.store[RESOURCE_ENERGY] !== 0);
+    if (!spawnStateMachine) {
+        spawnStateMachine = initSpawnStateMachine();
+        creepStateMachine = initCreepStateMachine();
+        return;
+    }
 
-    if (harvesters.length < 3) {
-        const c = spawn.spawnCreep([MOVE, MOVE, WORK, CARRY, CARRY, CARRY]).object;
+    spawnStateMachine.tick();
+    creepStateMachine.tick();
+}
+
+// SpawnState is a base class with access to a spawn point.
+class SpawnState {
+    constructor(spawn) {
+        this.spawn = spawn;
+    }
+}
+
+// SpawnStateHarvester spawns harvesters.
+class SpawnStateHarvester extends SpawnState {
+    tick() {
+        const c = this.spawn.spawnCreep([MOVE, MOVE, WORK, CARRY, CARRY, CARRY]).object;
         if (c) {
-            c.job = "harvester";
-            return;
+            c.job = ROLE_HARVESTER
         }
     }
+}
 
-
-    for (let i = 0; i < harvesters.length; i++) {
-        const harvester = harvesters[i];
-
-        // find closest container in case we end up on the other side of the map.
-        const container = findClosestByPath(harvester, containers);
-
-        if (harvester.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-            // if there are no containers around, there's no energy to collect.
-            if (!container) {
-                continue;
-            }
-
-            if (harvester.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                harvester.moveTo(container);
-            }
-        } else {
-            if (harvester.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                harvester.moveTo(spawn);
-            }
+// SpawnStateRangedAttacker spawns ranged attackers.
+class SpawnStateRangedAttacker extends SpawnState {
+    tick() {
+        const c = this.spawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]).object;
+        if (c) {
+            c.job = ROLE_RANGED_ATTACKER;
         }
     }
+}
 
-    const attacker = spawnRangedAttacker(spawn);
+class CreepStateGatherInitialResources {
+    tick() {
 
-    const availableSquad = squads.find(s => !s.ready());
-    if (!availableSquad && attacker) {
-        // create a new squad with the attacker and defined behaviour.
-        squads.push(new Squad([attacker], squadNum, creep => {
-            const closestEnemy = findClosestByPath(creep, enemyCreeps);
+        const spawn = getObjectsByPrototype(StructureSpawn).find(s => s.my);
+        const harvesters = getObjectsByPrototype(Creep).filter(c => c.my && c.job === ROLE_HARVESTER);
+        const rangedAttackers = getObjectsByPrototype(Creep).filter(c => c.my && c.job === ROLE_RANGED_ATTACKER);
+        const enemyCreeps = getObjectsByPrototype(Creep).filter(c => !c.my);
+        const containers = getObjectsByPrototype(StructureContainer).filter(c => c.store[RESOURCE_ENERGY] <= c.store.getCapacity() && c.store[RESOURCE_ENERGY] !== 0);
+        const enemySpawn = getObjectsByPrototype(StructureSpawn).find(s => !s.my);
+
+        // handler harvesters.
+        for (let harvester of harvesters) {
+            // find closest container in case we end up on the other side of the map.
+            const container = findClosestByPath(harvester, containers);
+
+            if (harvester.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                // if there are no containers around, there's no energy to collect.
+                if (!container) {
+                    continue;
+                }
+
+                if (harvester.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    harvester.moveTo(container);
+                }
+            } else {
+                if (harvester.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    harvester.moveTo(spawn);
+                }
+            }
+        }
+
+        // handler ranged attackers.
+        for (let rangedAttacker of rangedAttackers) {
+            const closestEnemy = findClosestByPath(rangedAttacker, enemyCreeps);
             const target = closestEnemy === null ? enemySpawn : closestEnemy;
-            if (creep.rangedAttack(target) === ERR_NOT_IN_RANGE) {
-                // yolo attack enemy spawn, doesn't account for any defences.
-                // if (creep.hits < creep.hitsMax) {
-                //     creep.heal(attacker);
-                // } else {
-                creep.moveTo(target);
-                // }
+            if (rangedAttacker.rangedAttack(target) === ERR_NOT_IN_RANGE) {
+                rangedAttacker.moveTo(target);
             }
-        }));
-    } else {
-        // add an attacker to an existing squad.
-        if (attacker) {
-            availableSquad.addMember(attacker);
-        }
-    }
-
-    for (const squad of squads) {
-        if (squad.ready()) {
-            squad.moveOut();
         }
     }
 }
 
-function spawnRangedAttacker(spawn) {
-    const c = spawn.spawnCreep([MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]).object;
-    if (c) {
-        c.job = "attacker";
-    }
-    return c;
-}
-
-class Squad {
-    constructor(creeps, desiredNum, fn) {
-        this.name = "squad-" + getTicks();
-        this.creeps = creeps;
-        this.desiredNum = desiredNum;
-        this.run = fn;
-        this.movedOut = false;
+// StateMachine enables the setting transition functions and types that implement a "tick" method.
+// the state machine will call the first tick function whose "conditionFn" returns true.
+class StateMachine {
+    constructor() {
+        this.states = [];
+        this.lastState = "";
     }
 
-    ready() {
-        return this.movedOut || this.creeps.length >= this.desiredNum;
+    setStateTransition(stateName, conditionFn, ticker) {
+        this.states.push({
+            name: stateName,
+            conditionFn: conditionFn,
+            ticker: ticker
+        })
     }
 
-    numMembers() {
-        return this.creeps.length;
-    }
-
-    addMember(creep) {
-        if (creep.squad) {
-            console.log(`tried to add creep to squad ${this.name} when it was already in squad ${creep.squad}`);
-            return;
+    tick() {
+        for (let state of this.states) {
+            if (state.conditionFn()) {
+                if (state.name !== this.lastState) {
+                    this.lastState = state.name;
+                    console.log("executing state: " + state.name);
+                }
+                state.ticker.tick();
+                return;
+            }
         }
-        creep.squad = this.name;
-        this.creeps.push(creep);
-    }
-
-    moveOut() {
-        this.movedOut = true;
-        for (const creep of this.creeps) {
-            this.run(creep);
-        }
-    }
-
-    setRun(fn) {
-        this.run = fn;
+        console.error("no conditionFn returned true, invalid wiring of this state machine.");
     }
 }
